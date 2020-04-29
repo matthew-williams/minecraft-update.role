b5721921c037a507d026b4e66dd25f3181aef019
matthew.williams <matthew.williams@programmer.net>
2020-04-29 15:59:18 -0600

    Updated quite a bit, remove unused flags, added restore option, Linted

--- files/minecraftupdate.sh
+++ files/minecraftupdate.sh
@@ -39,9 +39,8 @@ ERROR_OUT=/dev/stderr
 
 # Parameter Flags
 FLAG_NEWPACK=
-FLAG_CONFIRM=
 BACKUP_ONLY='0'
-FLAG_TEST='0'
+RESTORE_ONLY='0'
 
 # Help Page
 usage()
@@ -54,16 +53,15 @@ usage()
 	echo "Dependencies:"
 	echo "    cURL                 Included in most major distributions package repositories"
 	echo "    jq                   Check your distribution repositories or build from source"
-  echo "    unzip                Required in order to unzip the package"
+    echo "    unzip                Required in order to unzip the package"
 	echo
 	echo "Usage: $0 [options]"
 	echo
-	echo "    -y, --yes            Skip update confirmation"
 	echo "    -n, --newpack        Specify the location of the new pack .zip file"
 	echo "    -b, --backup         Backup only, no new pack will be installed"
+	echo "    -r, --restore        Restore only, no new pack will be installed"
 	echo "    --temp-dir           Specify a different temporary directory, default is $TEMP_DIR"
 	echo "    --pack-path          Specify a different final pack path folder, where the server is installed you want to update, default is $PACK_PATH"
-	echo "    -t                   Tests the script, will not remove current server files or replace them. WILL STILL REMOVE 7 DAY OR OLDER BACKUPS"
 	echo "    -h, --help           Print this help message"
 	echo
 	echo "Examle Usage:"
@@ -81,18 +79,15 @@ while [ "$1" != "" ]; do
 		-n | --newpack )	shift
 					FLAG_NEWPACK=$1
 					;;
-		-y | --yes )		FLAG_CONFIRM='1'
-					;;
 		-b | --backup )		BACKUP_ONLY='1'
-					FLAG_CONFIRM='1'
+					;;
+        -r | --restore )		RESTORE_ONLY='1'
 					;;
 		--temp-dir )		shift
 					TEMP_DIR=$1
 					;;
 		--pack-path )		shift
 					PACK_PATH=$1
-					;;
-		-t | --test )		FLAG_TEST='1'
 					;;
 		-h | --help )		usage
 					exit
@@ -105,27 +100,54 @@ while [ "$1" != "" ]; do
 	shift
 done
 
-DATE='date +%y_%m_%d_%H%M%S'
 if [[ "$BACKUP_ONLY" == "1" ]];
 then
 	echo "----BACK UP ONLY----" > $NORMAL_OUT
 fi
-mkdir -p $PACK_PATH
-mkdir -p $TEMP_DIR
+if [[ "$RESTORE_ONLY" == "1" ]];
+then
+	echo "----RESTORE ONLY----" > $NORMAL_OUT
+fi
+mkdir -p "$PACK_PATH"
+mkdir -p "$TEMP_DIR"
 echo "Removing old backups in $TEMP_DIR" > $NORMAL_OUT
-find ${TEMP_DIR}/ -type f -mtime +7 -delete
+find "${TEMP_DIR}"/ -type f -mtime +7 -delete
 echo "----Done----" > $NORMAL_OUT
 echo "Backing up $PACK_PATH" > $NORMAL_OUT
-rsync -vph ${PACK_PATH}server.properties ${TEMP_DIR}
-if [ -e ${PACK_PATH}ops.json ]
+rsync -vph "${PACK_PATH}"server.properties "${TEMP_DIR}"
+if [ -e "${PACK_PATH}"ops.json ]
 then
-	rsync -vph ${PACK_PATH}ops.json ${TEMP_DIR}
-	rsync -vph ${PACK_PATH}whitelist.json ${TEMP_DIR}
-	rsync -rvph ${PACK_PATH}backups ${TEMP_DIR}
-	rsync -rvph ${PACK_PATH}world ${TEMP_DIR}
+	rsync -vph "${PACK_PATH}"ops.json "${TEMP_DIR}"
+else
+    echo "----No ops.json found----" > $NORMAL_OUT
 fi
-rsync -vph ${PACK_PATH}*.txt ${TEMP_DIR}
-rsync -vph ${PACK_PATH}settings.sh ${TEMP_DIR}
+if [ -e "${PACK_PATH}"whitelist.json ]
+then
+	rsync -vph "${PACK_PATH}"whitelist.json "${TEMP_DIR}"
+else
+    echo "----No whitelist.json found----" > $NORMAL_OUT
+fi
+if [ -e "${PACK_PATH}"backups ]
+then
+	rsync -rvph "${PACK_PATH}"backups "${TEMP_DIR}"
+else
+    echo "----No backups folder found----" > $NORMAL_OUT
+fi
+if [ -e "${PACK_PATH}"world ]
+then
+	rsync -rvph "${PACK_PATH}"world "${TEMP_DIR}"
+else
+    echo "----No world folder found----" > $ERROR_OUT
+    echo "----Something when wrong----" > $ERROR_OUT
+    exit 1
+fi
+rsync -vph "${PACK_PATH}"*.txt "${TEMP_DIR}"
+if [ -e "${PACK_PATH}"settings.sh ]
+then
+    rsync -vph "${PACK_PATH}"settings.sh "${TEMP_DIR}"
+else
+    echo "----No settings.sh found----" > $NORMAL_OUT
+fi
 echo "----Done----" > $NORMAL_OUT
 if [[ "$BACKUP_ONLY" == "1" ]];
 then
@@ -133,26 +155,25 @@ then
 	echo "----BACKUP FLAG:$BACKUP_ONLY----" > $NORMAL_OUT
 else
 	echo "Replacing files from $PACK_PATH" > $NORMAL_OUT
-	if [ -e $FLAG_NEWPACK ]
+	if [ -e "$FLAG_NEWPACK" ]
 	then
-		rm -R $PACK_PATH
-		mkdir -p $PACK_PATH
-		unzip -oqD $FLAG_NEWPACK -d $PACK_PATH
+		rm -R "$PACK_PATH"
+		mkdir -p "$PACK_PATH"
+		unzip -oqD "$FLAG_NEWPACK" -d "$PACK_PATH"
 		echo "----Done----" > $NORMAL_OUT
 	  echo "Returning the backup files from $PACK_PATH" > $NORMAL_OUT
-		rsync -vph ${TEMP_DIR}server.properties ${PACK_PATH}
-		rsync -vph ${TEMP_DIR}*.txt ${PACK_PATH}
-		rsync -vph ${TEMP_DIR}settings.sh ${PACK_PATH}
-		rsync -vph ${TEMP_DIR}ops.json ${PACK_PATH}
-		rsync -vph ${TEMP_DIR}whitelist.json ${PACK_PATH}
-		rsync -rvph ${TEMP_DIR}backups ${PACK_PATH}
-		rsync -rvph ${TEMP_DIR}world ${PACK_PATH}
+		rsync -vph "${TEMP_DIR}"server.properties "${PACK_PATH}"
+		rsync -vph "${TEMP_DIR}"*.txt "${PACK_PATH}"
+		rsync -vph "${TEMP_DIR}"settings.sh "${PACK_PATH}"
+		rsync -vph "${TEMP_DIR}"ops.json "${PACK_PATH}"
+		rsync -vph "${TEMP_DIR}"whitelist.json "${PACK_PATH}"
+		rsync -rvph "${TEMP_DIR}"backups "${PACK_PATH}"
+		rsync -rvph "${TEMP_DIR}"world "${PACK_PATH}"
 		echo "----Done----" > $NORMAL_OUT
-		chown -R minecraft:minecraft $PACK_PATH
+		chown -R minecraft:minecraft "$PACK_PATH"
 		echo "----FINISHED UPDATING----" > $NORMAL_OUT
 	else
 		echo "This: $FLAG_NEWPACK does not exist!!" > $ERROR_OUT
 		exit 1
 	fi
 fi
-
